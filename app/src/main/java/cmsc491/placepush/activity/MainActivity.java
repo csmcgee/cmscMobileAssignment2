dbb0591b92b22726461a79cd90e2b673af38f524
Chris McGee <cmcgee.csm@gmail.com>
2015-04-18 17:29:33 -0400

    Foundation for search ability added.

--- app/src/main/java/cmsc491/placepush/activity/MainActivity.java
+++ app/src/main/java/cmsc491/placepush/activity/MainActivity.java
@@ -1,14 +1,19 @@
-package cmsc491.placepush;
+package cmsc491.placepush.activity;
 
+import android.app.SearchManager;
 import android.content.Context;
+import android.content.Intent;
 import android.location.Location;
 import android.location.LocationListener;
 import android.location.LocationManager;
 import android.support.v4.app.FragmentActivity;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.SearchView;
+import android.widget.Toast;
 
 import com.google.android.gms.maps.CameraUpdate;
 import com.google.android.gms.maps.CameraUpdateFactory;
@@ -19,13 +24,16 @@ import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
 
+import cmsc491.placepush.R;
 
-public class MainActivity extends FragmentActivity implements OnMapReadyCallback{
+
+public class MainActivity extends ActionBarActivity implements OnMapReadyCallback {
 
     public LocationManager locationManager;
     public MapLocationListener mapLocationListener = new MapLocationListener();
     private GoogleMap gMap;
     private Marker posMarker;
+    private ActionBar mActionBar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -35,6 +43,30 @@ public class MainActivity extends FragmentActivity implements OnMapReadyCallback
         locationManager = (LocationManager) this.getSystemService(Context.LOCATION_SERVICE);
         // minimum 25 meters, every 5 seconds
         locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 5000, 25, mapLocationListener);
+
+        // Action bar configurations
+        mActionBar = getSupportActionBar();
+        mActionBar.setDisplayShowHomeEnabled(true);
+        mActionBar.setLogo(R.mipmap.ic_launcher);
+        mActionBar.setDisplayUseLogoEnabled(true);
+
+        // intent handling
+        handleIntent(getIntent());
+
+    }
+
+    private void handleIntent(Intent intent){
+        // Handle Search Intent
+        if(intent.ACTION_SEARCH.equals(intent.getAction())){
+            String query = intent.getStringExtra(SearchManager.QUERY);
+            // do something with query
+        }
+    }
+
+    @Override
+    protected void onNewIntent(Intent intent){
+        setIntent(intent);
+        handleIntent(intent);
     }
 
     private void intializeMap(){
@@ -48,6 +80,13 @@ public class MainActivity extends FragmentActivity implements OnMapReadyCallback
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.menu_main, menu);
+
+        // Get the SearchView and set the searchable configuration
+        SearchManager searchManager = (SearchManager) getSystemService(Context.SEARCH_SERVICE);
+        SearchView searchView = (SearchView) menu.findItem(R.id.action_search).getActionView();
+        // Assumes current activity is the searchable activity
+        searchView.setSearchableInfo(searchManager.getSearchableInfo(getComponentName()));
+        searchView.setIconifiedByDefault(true); // Do not iconify the widget; expand it by default
         return true;
     }
 
@@ -59,7 +98,7 @@ public class MainActivity extends FragmentActivity implements OnMapReadyCallback
         int id = item.getItemId();
 
         //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
+        if (id == R.id.action_search) {
             return true;
         }
 
